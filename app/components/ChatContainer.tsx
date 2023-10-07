33e840c2c48276130d9f8fab333672146510abcd
Raffaele Brivio <raffaele.brivio@dev4side.com>
2023-10-07 23:17:52 +0200

    Modify userlist style

--- app/components/ChatContainer.tsx
+++ app/components/ChatContainer.tsx
@@ -15,11 +15,11 @@ export const ChatContainer = ({encryptionKeyPair}: IChatContainer) => {
   return (
     <Grid container spacing={2}>
 
-        <Grid item xs={12} md={3}>
+        <Grid item xs={12} md={4}>
             <UserList setAlert={setAlert} />
         </Grid>
 
-        <Grid item xs={12} md={9}>
+        <Grid item xs={12} md={8}>
             <Chat 
                 recipient={recipient}
                 encryptionKeyPair={encryptionKeyPair}
--- app/components/UserList.tsx
+++ app/components/UserList.tsx
@@ -1,100 +1,108 @@
-import { Stack, TextField, Typography, InputAdornment, IconButton, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Button, Box } from '@mui/material'
-import Avvvatars from 'avvvatars-react'
-import React, { Dispatch, SetStateAction } from 'react'
-import SendIcon from '@mui/icons-material/Send';
+import {
+  Stack,
+  TextField,
+  Typography,
+  InputAdornment,
+  IconButton,
+  Paper,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  ListItemSecondaryAction,
+  Button,
+  Divider,
+  ListSubheader,
+} from "@mui/material";
+import Avvvatars from "avvvatars-react";
+import React, { Dispatch, SetStateAction } from "react";
+import SendIcon from "@mui/icons-material/Send";
+import { truncateString } from "../utils";
 
 export interface IUserList {
-    setAlert: Dispatch<SetStateAction<boolean>>
+  setAlert: Dispatch<SetStateAction<boolean>>;
 }
 
-export const UserList = ({setAlert}: IUserList) => {
+export const UserList = ({ setAlert }: IUserList) => {
+  const [contractAddress, setContractAddress] = React.useState<string>("");
 
-    const [contractAddress, setContractAddress] = React.useState<string>('');
+  const setUserToLocalStorage = () => {
+    if (contractAddress.length !== 42 && contractAddress.length !== 0) return;
+    localStorage.setItem("dissent-contractAddress", contractAddress);
+    setAlert(true);
+  };
 
-    const setUserToLocalStorage = () => {
-        if (contractAddress.length !== 42 && contractAddress.length !== 0) return;
-        localStorage.setItem('dissent-contractAddress', contractAddress);
-        setAlert(true);
-    };
+  function createData(address: string) {
+    return { address };
+  }
 
-    function createData(
-        users: string,
-    ) {
-        return { users };
-    }
-    
-    const rows = [
-        createData('Frozen yoghurt'),
-        createData('Ice cream sandwich'),
-        createData('Eclair'),
-        createData('Cupcake'),
-        createData('Gingerbread'),
-    ];
+  const rows = [
+    createData("Frozen yoghurt"),
+    createData("Ice cream sandwich"),
+    createData("Eclair"),
+    createData("Cupcake"),
+    createData("Gingerbread"),
+  ];
 
   return (
-    <Stack gap={2} alignItems='center'>
-        
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Contranct address</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-
-            <TableRow
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-                <TableCell component="th" scope="row">
-                    <TextField
-                        onChange={(event) => setContractAddress(event.target.value)}
-                        error={contractAddress.length !== 42 && contractAddress.length !== 0}
-                        helperText={ contractAddress.length !== 42 && contractAddress.length !== 0 &&
-                            <Typography>Please insert a valid contract address</Typography>
-                        }
-                        fullWidth 
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={setUserToLocalStorage}
-                                        edge="end"
-                                    >
-                                        <SendIcon />
-                                    </IconButton>
-                                </InputAdornment>
-                            ),
-                        }}
-                        variant='outlined'
-                        placeholder='Insert a contract address'
-                    />
-                </TableCell>
-            </TableRow>
-
-
-              {rows.map((row) => (
-                <TableRow
-                  key={row.users}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    <Stack gap={2} alignItems="center">
+      {/* <Paper>
+        <TextField
+          onChange={(event) => setContractAddress(event.target.value)}
+          error={contractAddress.length !== 42 && contractAddress.length !== 0}
+          helperText={
+            contractAddress.length !== 42 &&
+            contractAddress.length !== 0 && (
+              <Typography>Please insert a valid contract address</Typography>
+            )
+          }
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={setUserToLocalStorage}
+                  edge="end"
                 >
-                  <TableCell component="th" scope="row">
-                    <Stack direction='row' alignItems='center' justifyContent='space-between' >
-                        <Stack direction='row' alignItems='center' gap={2}>
-                            <Avvvatars style='shape' value={row.users} />
-                            {row.users}
-                        </Stack>
-                        <Button>
-                            CHAT
-                        </Button>
-                    </Stack>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Stack>
-  )
-}
+                  <SendIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          variant="outlined"
+          placeholder="Insert a contract address"
+        />
+      </Paper> */}
+
+      <List
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            <Typography variant="h6" align="center" gutterBottom>
+              📢 Active dissidents
+            </Typography>
+          </ListSubheader>
+        }
+      >
+        {rows.map((row, index) => (
+          <React.Fragment key={row.address}>
+            <ListItem
+              sx={{ "&:last-child": { border: 0 } }}
+              button // Add the 'button' property to make the ListItem clickable
+              onClick={() => {
+                // Handle the click event here
+                console.log("ListItem clicked:", row.address);
+              }}
+            >
+              <ListItemAvatar>
+                <Avvvatars style="shape" value={row.address} />
+              </ListItemAvatar>
+              <ListItemText primary={truncateString(row.address, 8, 6)} />
+            </ListItem>
+            {index < rows.length - 1 && <Divider />}
+          </React.Fragment>
+        ))}
+      </List>
+    </Stack>
+  );
+};
