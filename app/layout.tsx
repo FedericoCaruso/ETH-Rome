a8d4a91da3dbd7821605de54c89c1d6d56eccac9
Raffaele Brivio <raffaele.brivio@dev4side.com>
2023-10-07 17:01:42 +0200

    Initialize waku light node

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { MetaMaskProvider } from 'metamask-react'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
+import { LightNode } from '@waku/interfaces'
+import { initWaku } from './utils/waku'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +15,22 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const [waku, setWaku] = useState<LightNode>();
+
+  // Waku initialization
+  useEffect(() => {
+    (async () => {
+      if (waku) return;
+
+      const _waku = await initWaku();
+      console.log("waku: ready");
+      setWaku(_waku);
+    })().catch((e) => {
+      console.error("Failed to initiate Waku", e);
+    });
+  }, [waku]);
+
+
   return (
     <html lang="en">
       <body className={inter.className}>
